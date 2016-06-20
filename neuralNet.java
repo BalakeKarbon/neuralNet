34cb1b47753f29382f26ceffab4688bdf09eca18
BalakeKarbon <blakekarbon@gmail.com>
2016-06-19 23:20:32 -0600

    Improved the IO of the demo program.

--- neuralNet.java
+++ neuralNet.java
@@ -179,9 +179,13 @@ class neuralNet {
 		Random random = new Random();
 		//Generate and output nerual network structure.
 		//Structure:
-		int mahStructure[] = {2,5,1};
+		int mahStructure[] = {2,5,5,1};
+		int mahWeightCount = 0;
 		System.out.println("Neural Network Structure:");
 		for(int currentMahLayer = 0;currentMahLayer<mahStructure.length;currentMahLayer++) {
+			if(currentMahLayer>0) {
+				mahWeightCount+=(mahStructure[currentMahLayer]*mahStructure[currentMahLayer-1]);
+			}
 			System.out.println(" Layer " + (currentMahLayer+1) + ":");
 			if(currentMahLayer == 0) {
 				System.out.println("  Type: Input");
@@ -195,14 +199,14 @@ class neuralNet {
 			}
 		}
 		//Weights:
-		double mahInitialWeights[] = new double[15]; //EX:2 * 5 for first layer, 5 * 5 for second, 5 * 1 for last. {2,5,5,1}
+		double mahInitialWeights[] = new double[mahWeightCount]; //EX:2 * 5 for first layer, 5 * 5 for second, 5 * 1 for last. {2,5,5,1}
 		System.out.println(" Weights:");
 		for(int currentInitialWeight = 0;currentInitialWeight<mahInitialWeights.length;currentInitialWeight++) {
-			mahInitialWeights[currentInitialWeight] = (double)((random.nextDouble() * 0.7)+0.3);
+			mahInitialWeights[currentInitialWeight] = (double)((random.nextDouble() * 0.5)+0.5);
 			System.out.println("  w" + (currentInitialWeight+1) + ": " + mahInitialWeights[currentInitialWeight]);
 		}
 		//Bias Values:
-		double mahBiasValues[] = {1.f,1.f};
+		double mahBiasValues[] = {1.f,1.f,1.f};
 		System.out.println(" Bias Values:");
 		for(int currentBiasValue = 0;currentBiasValue<mahBiasValues.length;currentBiasValue++) {
 			System.out.println("  Layer " + (currentBiasValue+1) + " bias value: " + mahBiasValues[currentBiasValue]);
@@ -212,12 +216,17 @@ class neuralNet {
 		//Make sure the nerual network initialized without any errors.
 		if(mahANN.isInitializedCorrectly()) {
 			System.out.println("\nThe ANN initialized correctly!");
-			double mahLearningConstant = 1;
+			double mahLearningConstant = 0.1;
 			double annInputs[] = new double[2];
 			double annDesiredOutputs[] = new double[1];
 			double mahTotalError = 1;
 			long mahTrains = 0;
-			while(mahTotalError>0.01) {
+			long currentTime = System.currentTimeMillis();
+			long lastTime = System.currentTimeMillis();
+			long startTime = currentTime;
+			double lastError = 1;
+			double requiredError = 0.01;
+			while(mahTotalError>requiredError) {
 				mahTrains++;
 				mahTotalError = 0;
 				annInputs[0] = 0.f;
@@ -237,18 +246,28 @@ class neuralNet {
 				annDesiredOutputs[0] = 0.f;
 				mahTotalError+=mahANN.train(annInputs,annDesiredOutputs,mahLearningConstant);
 				mahTotalError=mahTotalError/4;
-				//System.out.println(mahTotalError);
-				/*System.out.println("New Weights:");
-				for(int mahCurWeight = 0;mahCurWeight<mahANN.getWeights().length;mahCurWeight++) {
-					System.out.println(" w"+mahCurWeight+": "+mahANN.getWeights()[mahCurWeight]);
-				}*/
+				currentTime = System.currentTimeMillis();
+				if((currentTime-lastTime)>=1000) {
+					lastTime = currentTime;
+					System.out.println("Generation " + mahTrains + ":");
+					System.out.println(" Total Error:" + mahTotalError);
+					System.out.println(" New Weights:");
+					for(int mahCurWeight = 0;mahCurWeight<mahANN.getWeights().length;mahCurWeight++) {
+						System.out.println("  w"+mahCurWeight+": "+mahANN.getWeights()[mahCurWeight]);
+					}
+					System.out.println(" Improvement:" + (lastError-mahTotalError));
+					lastError=mahTotalError;
+				}
 			}
-			System.out.println(mahTrains);
-			System.out.println(mahTotalError);
-			System.out.println("New Weights:");
+			System.out.println("Generation " + mahTrains + ":");
+			System.out.println(" Total Error:" + mahTotalError);
+			System.out.println(" New Weights:");
 			for(int mahCurWeight = 0;mahCurWeight<mahANN.getWeights().length;mahCurWeight++) {
-				System.out.println(" w"+mahCurWeight+": "+mahANN.getWeights()[mahCurWeight]);
+				System.out.println("  w"+mahCurWeight+": "+mahANN.getWeights()[mahCurWeight]);
 			}
+			System.out.println(" Improvement: " + (lastError-mahTotalError));
+			long totalElapsedTime = (System.currentTimeMillis()-startTime);
+			System.out.println("Total elapsed time: " + totalElapsedTime + " milliseconds.");
 		} else {
 			mahANN = null;
 			System.gc(); //Call the garbage collector.
